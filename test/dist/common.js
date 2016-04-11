e18107ab47fcfda12eb98de4e96012766f1432e8
Brandon Kitajchuk <kitajchuk@gmail.com>
2016-04-11 11:31:10 -0700

    Spring cleaning - tighten it up

--- test/dist/common.js
+++ test/dist/common.js
@@ -67,41 +67,20 @@
 	 *
 	 */
 	(function ( factory ) {
-	    
+
 	    if ( true ) {
 	        module.exports = factory();
 
 	    } else if ( typeof window !== "undefined" ) {
 	        window.KonamiCode = factory();
 	    }
-	    
+
 	})(function () {
 
 
-	    var _keys = {
-	            A: 65,
-	            B: 66,
-	            UP: 38,
-	            DOWN: 40,
-	            LEFT: 37,
-	            RIGHT: 39
-	        },
-	        
-	        _code = [
-	            _keys.UP,
-	            _keys.UP,
-	            _keys.DOWN,
-	            _keys.DOWN,
-	            _keys.LEFT,
-	            _keys.RIGHT,
-	            _keys.LEFT,
-	            _keys.RIGHT,
-	            _keys.B,
-	            _keys.A
-	            
-	        ].join( "" );
-	    
-	    
+	    var _code = "38384040373937396665";
+
+
 	    /**
 	     *
 	     * A konami code easter egg handler
@@ -112,10 +91,11 @@
 	    var KonamiCode = function () {
 	        return this.init.apply( this, arguments );
 	    };
-	    
+
+
 	    KonamiCode.prototype = {
 	        constructor: KonamiCode,
-	        
+
 	        /**
 	         *
 	         * KonamiCode init constructor method
@@ -132,7 +112,7 @@
 	             *
 	             */
 	            this._delay = 500;
-	            
+
 	            /**
 	             *
 	             * All supplied callbacks to this instance
@@ -141,7 +121,7 @@
 	             *
 	             */
 	            this._callbacks = [];
-	            
+
 	            /**
 	             *
 	             * Timeout reference
@@ -150,37 +130,32 @@
 	             *
 	             */
 	            this._timeout = null;
-	            
+
 	            var code = "",
 	                self = this,
 	                handler = function ( e ) {
 	                    try {
 	                        clearTimeout( self._timeout );
-	                        
+
 	                    } catch ( error ) {}
-	                    
-	                    code = "" + (code + e.keyCode);
-	                    
+
+	                    code = String( code + e.keyCode );
+
 	                    if ( code === _code ) {
-	                        self._dispatch( "konami-code" );
+	                        self._dispatch( "konami" );
 	                    }
-	                    
+
 	                    self._timeout = setTimeout(function () {
 	                        clearTimeout( self._timeout );
-	                        
+
 	                        code = "";
-	                                    
+
 	                    }, self._delay );
 	                };
-	            
-	            if ( document.addEventListener ) {
-	                document.addEventListener( "keydown", handler, false );
-	                
-	            } else if ( document.attachEvent ) {
-	                document.attachEvent( "onkeydown", handler );
-	            }
+
+	            document.addEventListener( "keydown", handler, false );
 	        },
-	        
+
 	        /**
 	         *
 	         * Listen for the konami code input
@@ -193,26 +168,25 @@
 	            if ( typeof callback === "function" ) {
 	                this._callbacks.push( callback );
 	            }
-	            
+
 	            return this;
 	        },
-	        
+
 	        /**
 	         *
 	         * Internal dispatcher when konami code input is matched
 	         * @memberof KonamiCode
 	         * @method KonamiCode._dispatch
-	         * @param {string} event The _konamicode_ event string
 	         *
 	         */
-	        _dispatch: function ( event ) {
+	        _dispatch: function () {
 	            for ( var i = this._callbacks.length; i--; ) {
-	                this._callbacks[ i ].call( this, event );
+	                this._callbacks[ i ]();
 	            }
 	        }
 	    };
-	    
-	    
+
+
 	    return KonamiCode;
 
 
